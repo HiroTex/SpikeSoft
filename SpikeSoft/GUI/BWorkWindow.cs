d9061bea442bc19240f18350bf3f3d209b311a23
HiroTex <BeaterMaster@outlook.com>
2023-04-06 16:01:27 -0300

    Task Manager

---
+++ SpikeSoft/GUI/BWorkWindow.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpikeSoft.GUI
+{
+    public partial class BWorkWindow : Form
+    {
+        bool ShowCompleteDialog = true;
+
+        public BWorkWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SetLabel(string text)
+        {
+            ProgressLabel.Text = text;
+        }
+
+        public void SetProgressValue(int value)
+        {
+            if (value > 100)
+            {
+                pBar.Maximum = value;
+            }
+
+            pBar.Value = value;
+        }
+
+        public void InitializeNewThread(DoWorkEventHandler work, object[] args, string Title, bool ShowCompleteDialog)
+        {
+            pBar.Maximum = 100;
+            SetLabel(Title);
+            this.ShowCompleteDialog = ShowCompleteDialog;
+
+            var bw = new BackgroundWorker();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.ProgressChanged += ReportProgress;
+            bw.RunWorkerCompleted += ProcessCompleted;
+            bw.DoWork += work;
+
+            Thread.Sleep(500);
+
+            bw.RunWorkerAsync(args);
+        }
+
+        public void ReportProgress(object sender, ProgressChangedEventArgs e)
+        {
+            SetProgressValue(e.ProgressPercentage);
+        }
+
+        public void ProcessCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.Hide();
+            if (ShowCompleteDialog) MessageBox.Show("The Task was Completed", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+    }
+}
